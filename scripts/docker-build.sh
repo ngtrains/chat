bfff1af225423eaf21f0928a73a22bcaad4e86b2
Nerijus Gedrimas <nerijus.gedrimas@gmail.com>
2026-03-20 09:24:50 +0200

    Switched to Debian

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t ngisme/chat -f ../docker/Dockerfile ../
+docker build --progress=plain  -t ngisme/chat -f ../docker/Dockerfile ../
--- scripts/init.sh
+++ scripts/init.sh
@@ -6,7 +6,7 @@ tmux rename-window -t 0 'git'
 tmux send-keys -t 'git' 'cd ~/Sources/chat/' C-m
 
 tmux new-window -t $session:1 -n 'docker'
-tmux send-keys -t 'docker' 'cd ~/Sources/chat/docker/' C-m
+tmux send-keys -t 'docker' 'cd ~/Sources/chat/scripts' C-m
 
 tmux attach -t $session:0
 
